b2bb18c7f76d13f0a07a88ef753f1f47bb81da2e
BSDC <seo17381738@dgsw.hs.kr>
2026-06-24 13:52:44 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/dormitory/merit/MeritApplication.java
@@ -0,0 +1,13 @@
+package com.dormitory.merit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeritApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeritApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dormitory/merit/MeritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dormitory.merit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeritApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
